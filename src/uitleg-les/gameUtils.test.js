561fa0f10478ad4cf4ae535045f84b092602621e
9024454 <9024454@student.zadkine.nl>
2026-03-05 12:15:04 +0100

    Todo Item Validatiec

---
+++ src/uitleg-les/gameUtils.test.js
@@ -0,0 +1,16 @@
+import {getRank, addScores, hasWon, getHighScore} from './gameUtils'
+
+test('Geef Noob terug met score 0', () => {
+    const result = getRank(0);
+    expect(result).toBe('Noob')
+})
+
+test('Geef Pro terug met score 55', () => {
+    const result = getRank(55);
+    expect(result).toBe('Pro')
+})
+
+test("tel twee scores bij elkaar op", () =>{
+    const result = addScores(20,30);
+    expect(result).toBe(50)
+})
